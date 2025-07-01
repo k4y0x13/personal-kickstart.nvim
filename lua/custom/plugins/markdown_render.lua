f8735f582419ec8526bf8a49e3aadca7892ba553
Kamal Acharya <kamalacharya.cst@rub.edu.bt>
2025-07-01 19:25:34 +0600

    feat: added markdown rendered plugin

---
+++ lua/custom/plugins/markdown_render.lua
@@ -0,0 +1,13 @@
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+  preview = {
+    icon_provider = 'devicons',
+  },
+
+  -- For blink.cmp's completion
+  -- source
+  -- dependencies = {
+  --     "saghen/blink.cmp"
+  -- },
+}
