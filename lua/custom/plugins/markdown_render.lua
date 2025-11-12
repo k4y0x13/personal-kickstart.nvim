beb7932600c732d4bf59c041a9e7101ce9173e2d
Kamal Acharya <kamalacharya.cst@rub.edu.bt>
2025-11-12 09:44:33 +0600

    hardtime and markdown renderer disable

--- lua/custom/plugins/markdown_render.lua
+++ lua/custom/plugins/markdown_render.lua
@@ -1,13 +1,15 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
+  enabled = false,
+  priority = 49,
   preview = {
     icon_provider = 'devicons',
   },
 
   -- For blink.cmp's completion
   -- source
-  -- dependencies = {
-  --     "saghen/blink.cmp"
-  -- },
+  dependencies = {
+    'saghen/blink.cmp',
+  },
 }
